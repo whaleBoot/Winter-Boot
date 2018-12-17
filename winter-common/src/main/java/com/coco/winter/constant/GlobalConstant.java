bff3603b76135374a2e5004b9556eb9a8ac3479f
like <like3168@pansoft.com>
2018-12-17 16:19:45 +0800

    全局异常统一处理

---
+++ winter-common/src/main/java/com/coco/winter/constant/GlobalConstant.java
@@ -0,0 +1,15 @@
+package com.coco.winter.constant;
+
+/**
+ * @ClassName GlobalConstant
+ * @Description 全局常量
+ * @Author like
+ * @Data 2018/12/6 10:00
+ * @Version 1.0
+ **/
+
+public interface GlobalConstant {
+
+    String CUSTOM_TOKEN="eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxIiwiaWF0IjoxNTI3NzUzOTI5LCJzdWIiOiIxLGFkbWluLDExMSIsImlzcyI6ImFkbWluIiwiZXhwIjoxNTI3NzU1NzI5fQ.eTGWB0OzXL5libsDXr5_9Gfj8XurKA1AvZO_xVTGUQs";
+
+}
